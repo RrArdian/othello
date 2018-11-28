bb087ecbe2eebbc9895ac383a9718b475cbb730f
Rais Rahman <rais@skyshi.com>
2018-11-28 10:30:21 +0700

    update activity

--- app/src/main/java/com/othello/test/controller/MainActivity.java
+++ app/src/main/java/com/othello/test/controller/MainActivity.java
@@ -2,12 +2,17 @@ package com.othello.test.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.othello.test.R;
 import com.othello.test.model.Board;
+import com.othello.test.model.Player;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Board board;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
         Board board = new Board();
         Toast.makeText(getApplicationContext(), "Initialize game!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickCell(View v) {
+        Button button = (Button) v;
+
+        String tag = button.getTag().toString();
+        int row = Integer.parseInt(tag.substring(0, 1));
+        int col = Integer.parseInt(tag.substring(1, 2));
+
+        Player movePlayer = board.mark(row, col);
     }
 }
